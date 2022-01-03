d402a322e6cf6b3752b9ef8c3948035a212bf30d
benmatrood <konateyaya526@gmail.com>
2022-01-03 23:56:00 +0000

    add_adminlte_install_livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+
+// import adminlte files from node_modules
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
